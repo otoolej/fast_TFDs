db097407b997a852aa08ed9b4af754aa04001102
John O' Toole <j.otoole@ieee.org>
2012-11-06 23:02:22 +0100

    copied from uqccr dir, where it was version 0.13b

---
+++ create_release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# create a release to a separate direction
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <release_number>"
+  exit 1
+fi
+
+RELEASE="$1";
+
+LONG_DIR='/home/otoolej/uqccr/work/software/discreteTFDs/dtfds_proposed';
+NEW_DIR=${LONG_DIR}/fastTFDs_releases/fastTFDs_v;
+NEW_DIR=${NEW_DIR}${RELEASE}
+
+
+# 1. make new directory
+mkdir $NEW_DIR
+
+# 2. tag current git release (make sure git is up-to-date first)
+if [ -n "$(git status --porcelain)" ]; then 
+  echo "git is not up to date !"; 
+  git status;
+  exit;
+else 
+  echo "git ok; no changes";
+fi
+
+git tag "release_v$RELEASE"
+
+
+# 3. copy all to new direction and clean-up
+cp -R * ${NEW_DIR}/
+rm -f ${NEW_DIR}/notes.org
+rm -f ${NEW_DIR}/create_release.sh
+rm -rf ${NEW_DIR}/.git
---
+++ utils/make_odd.m
@@ -0,0 +1,6 @@
+function y=make_odd(x)
+if(~rem(x,2))
+    y=x+1;
+else
+    y=x;
+end
